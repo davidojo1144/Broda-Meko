e7d25dced02d4a6a43a6939a8818c705d31f2eb5
davidojo1144 <davco1144@gmail.com>
2026-01-16 03:01:36 +0100

    Implementation

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-900">
-      <Ionicons name="home-outline" size={wp('12%')} color="#ffffff" />
-      <Text className="text-white text-xl font-semibold">Home</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-[#093275]" >
+      <View style={{ padding: wp(3) }}>
+        <Ionicons name="home-outline" size={wp('12%')} color="#ffffff" />
+        <Text className="text-white text-xl font-semibold">Home</Text>
+      </View>
+    </SafeAreaView >
   );
 }
---
+++ lib/Onboarding.ts
@@ -0,0 +1,18 @@
+
+export const Onboarding = [
+    {
+        img: "",
+        title: "Support When Your Car Stops",
+        Description: "Find nearby mechanics and roadside assistance when your car breaks down, anytime and anywhere.",
+    },
+    {
+        img: "",
+        title: "Verified mechanics Near you",
+        Description: "Connect with verified auto technicians around you, with clear ratings and reliable services",
+    },
+    {
+        img: "",
+        title: "Find Genuine Spare Parts",
+        Description: "Discover trusted spare parts sellers near you and get the right parts for your vehicle easily.",
+    },
+]
